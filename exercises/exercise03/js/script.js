58b10a1695b70e9b0b3300cddab312bc36938a4f
Gavin <gavin.e.park@gmail.com>
2018-01-23 17:16:30 -0500

    add exercise 03

--- exercises/exercise03/js/script.js
+++ exercises/exercise03/js/script.js
@@ -10,13 +10,11 @@ Description of what the script does...
 
 $(document).ready(function () {
 
-
+  // Insert jQuery code here to run when the page is loaded
   var counter = 0; 
   var updateInterval = 1000;  
-
   setInterval(function () { 
     counter = counter + 1;
     $('#counter').text(counter);
    },updateInterval);
-
 });
