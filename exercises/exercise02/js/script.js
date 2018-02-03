c731395e08c2b146be287daa95c73bf25e137c31
Gavin <gavin.e.park@gmail.com>
2018-02-02 21:44:43 -0500

    beginning of exercise04 - images

--- exercises/exercise02/js/script.js
+++ exercises/exercise02/js/script.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
-  // All our code goes in here!
 
   $('.faller').on('mouseover',function () {
 
     $(this).animate({
       opacity: '0',
-      top: '1000px'
+      top: '500px'
     },1500,function () {
       $(this).css({
         top: '0px'
       });
       $(this).animate({
         opacity: '1'
-      },1000);
+      },6000);
     });
   });
 
