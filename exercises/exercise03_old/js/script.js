c9099cc15add3c0915989b86b64c4e8b506ad94f
Gavin <gavin.e.park@gmail.com>
2018-02-03 21:53:09 -0500

    removed initial exercise 03, replaced it with new

---
+++ exercises/exercise03_old/js/script.js
@@ -0,0 +1,36 @@
+/**********************************************
+
+DART 450, Winter 2018
+Title of Project
+Author
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+=======
+>>>>>>> 58b10a1695b70e9b0b3300cddab312bc36938a4f
+=======
+>>>>>>> 58b10a1695b70e9b0b3300cddab312bc36938a4f
+  var counter = 0; 
+  var updateInterval = 1000;  
+  setInterval(function () { 
+    counter = counter + 1;
+    $('#counter').text(counter);
+   },updateInterval);
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 58b10a1695b70e9b0b3300cddab312bc36938a4f
+=======
+>>>>>>> 58b10a1695b70e9b0b3300cddab312bc36938a4f
+=======
+>>>>>>> 58b10a1695b70e9b0b3300cddab312bc36938a4f
+});
