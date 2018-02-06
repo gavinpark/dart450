ec433dca51ae01cd89c3f81b713c7d680080423a
Gavin <gavin.e.park@gmail.com>
2018-02-06 12:35:58 -0500

    divs hidden

--- exercises/exercise03/js/script.js
+++ exercises/exercise03/js/script.js
@@ -8,14 +8,14 @@ Description of what the script does...
 
 **********************************************/
 
-$(document).ready(function () {
+
 
   // Insert jQuery code here to run when the page is loaded
 
-var phrase01Delay = 100;
-setTimeout (function()
-$('#phrase01').fadeIn()
-}, phrase01Delay;
+  $(document).ready(function(){
 
-});
-});
+
+      $('div').fadeOut(1);
+      $('div').removeClass('hidden');
+      $('div').fadeIn(10000);
+  });
