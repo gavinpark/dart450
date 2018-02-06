f1c5289abc2f96a5973cd129d75d75ee2a7d43cd
gavinpark <gavin.e.park@gmail.com>
2018-02-06 17:18:46 -0500

    changes to jquery final image fade in

--- exercises/exercise04/js/script.js
+++ exercises/exercise04/js/script.js
@@ -10,24 +10,34 @@ Description of what the script does...
 
 $(document).ready(function () {
 
-      $(".futureImages").hide();
+  $(".futureImages").hide();
+  $("#finalImage").hide();
+  $("#finalWords").hide();
 
-      $("#futureLooks").click(function(){
-          $(".futureImages").show();
+  $("#futureLooks").click(function(){
+    $(".futureImages").show();
+    setTimeout (function(){
+      $("#finalImage").fadeIn(4000);
+    },6000)
+    setTimeout (function(){
+      $("#finalWords").fadeIn(4000);
+    },7000)
+
+    });
+
+  setInterval(function() {
+    $('.futureImages').each(function(){
+      var randomX = Math.random() * $(window).width();
+      var randomY = Math.random() * $(window).height();
+      $(this).css({
+        position: 'absolute',
+        top: randomY,
+        left: randomX
       });
-
-      setInterval(function() {
-           $('.futureImages').each(function(){
-             var randomX = Math.random() * $(window).width();
-             var randomY = Math.random() * $(window).height();
-             $(this).css({
-               position: 'absolute',
-               top: randomY,
-               left: randomX
-             });
-           });
-         }, 1500);
+    });
+  }, 1500);
 
 
 
-  });
+
+});
