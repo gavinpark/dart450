aafda9feea4a16dcc40490a24227c12131e27be6
Gavin <gavin.e.park@gmail.com>
2018-02-18 23:14:33 -0500

    more background colour checks

--- Projects /project01/js/script.js
+++ Projects /project01/js/script.js
@@ -10,17 +10,20 @@ Description of what the script does...
 
 $(document).ready(function () {
 
-if ($(window).width() < 400) {
-$('body').css({backgroundColor: 'blue'});
-}
-
-else if ($(window).width() <600) {
-  $('body').css({backgroundColor:'green'});
-}
-else {
-  $('body').css({backgroundColor: 'yellow'});
-
-
-};
+  if ($(window).width() < 200) {
+    $('body').css({ backgroundColor: 'red' });
+  }
+  else if ($(window).width() < 400) {
+    $('body').css({ backgroundColor: 'yellow' });
+  }
+  else if ($(window).width() < 600) {
+    $('body').css({ backgroundColor: 'green' });
+  }
+  else if ($(window).width() < 800) {
+    $('body').css({ backgroundColor: 'blue' });
+  }
+  else {
+    $('body').css({ backgroundColor: 'orange' });
+  }
 
 });
