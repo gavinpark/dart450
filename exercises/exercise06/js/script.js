db486ee33489f6c1f651586579d869fb9eea1ed3
Gavin <gavin.e.park@gmail.com>
2018-02-20 15:45:35 -0500

    Setting up exercise 06

--- exercises/exercise06/js/script.js
+++ exercises/exercise06/js/script.js
@@ -9,4 +9,14 @@ Pippin Barr
 $(document).ready(function() {
 
 
+$('#seriousText').blast({
+    delimiter: 'character'
+  }).mouseover(fadeToPink);
+
 });
+
+function fadeToPink () {
+  $(this).animate({
+      font-color: 'pink',
+  },1000);
+}
