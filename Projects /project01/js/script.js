12b020330bc7b20b0da65af84f79c16913eb6d8a
Gavin <gavin.e.park@gmail.com>
2018-02-23 00:57:53 -0500

    updated javascript for header02 & header03, notes added, still broken

--- Projects /project01/js/script.js
+++ Projects /project01/js/script.js
@@ -8,25 +8,25 @@ Gavin Park
 
 var backgroundFadeTime = 3000;
 var currentBackgroundColor = 0;
-var backgroundColors = ['#F8F8F8','#F0F0F0','#DCDCDC','#D3D3D3','#C8C8C8','BEBEBE','#B0B0B0','#A8A8A8','#989898','#888888','#787878','#696969','#606060','#505050','#404040','#303030','#202020','#101010'	]
+var backgroundColors = ['#F8F8F8','#F0F0F0','#DCDCDC','#D3D3D3','#C8C8C8','BEBEBE','#B0B0B0','#A8A8A8','#989898','#888888','#787878']
 
 $(document).ready(function() {
 
-//This calls a different colour after each 10 second interval.
+  //This calls a different colour after each 10 second interval.
   $('body').css({
     backgroundColor: backgroundColors[currentBackgroundColor]
   });
   fadeBackground();
   setInterval(fadeBackground,backgroundFadeTime);
 
-// This lets each header appear at different times of the day.
-// For the purposed of editing this code, the times are not currently set.
+  // This lets each header appear at different times of the day.
+  // For the purposed of editing this code, the times are not currently set and headers are visible 24/7.
 
   $('#header01').hide();
 
   var now = new Date();
   var hour = now.getHours();
-  if(hour>9 && hour<10){
+  if(hour>=0 && hour<24){
     fallingLetters();
   }
 
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
   var now = new Date();
   var hour = now.getHours();
-  if(hour>9 && hour<24){
+  if(hour>=0 && hour<24){
     wordFadeIn();
   }
 
@@ -42,7 +42,7 @@ $(document).ready(function() {
 
   var now = new Date();
   var hour = now.getHours();
-  if(hour>9 && hour<24){
+  if(hour>=0 && hour<24){
     riseFromBelow();
   }
 
@@ -52,10 +52,10 @@ $(document).ready(function() {
 
 function fallingLetters(){
   $('#header01').show();
-// Calling .blast() to our header01 element and separating the phare by letter.
-$('#header01').blast({
-  delimiter:'character'
-}).mouseover(animateToRandomLocation)
+  // Calling .blast() to our header01 element and separating the phare by letter.
+  $('#header01').blast({
+    delimiter:'character'
+  }).mouseover(animateToRandomLocation)
 }
 
 function fadeBackground(){
@@ -72,12 +72,12 @@ function fadeBackground(){
 function animateToRandomLocation () {
   var x = Math.floor(Math.random() * $(window).width());
 
-// Setting the position to be relative keeps the original position of each letter.
+  // Setting the position to be relative keeps the original position of each letter.
   $(this).css({
     position: 'relative'
   });
 
-// This animation sends each letter to a random location on the bottom of the page
+  // This animation sends each letter to a random location on the bottom of the page
   $(this).animate({
     left: x+'px',
     top:'2000px'
@@ -89,30 +89,46 @@ function animateToRandomLocation () {
   });
 
 }
-
+// This function will to let header02 appear during certain hours of the day.
+// I would like to have it initally be invisible and then fade in on mouseover.
 function wordFadeIn(){
+  $('#header02').show();
+  // This makes the starting opacity of the text invisible and location relative.
+  $(this).css({
+    opacity: '1'
+  });
+  // This breaks the header by letter and calls the function fadeIn with mouseover
   $('#header02').blast({
-    delimiter:'word'
+    delimiter:'character'
   }).mouseover(fadeIn)
-
 }
 
+// This brings the opacity of each span to 1 over a period of 3 secons
 function fadeIn(){
-  $(this).show()
-
+  $(this).animate({
+    opactiy: '1'
+  }, 3000);
 }
 
-// This function shows header03
+
+
+// This function will show header03 at certain hours of the day.
+// I would like to have it start aligned to the bottom of the page and then each letter floats to the middle of the screen on mouseover.
+
 function riseFromBelow(){
   $('#header03').show();
-// Calling .blast() to our header01 element and separating the phare by letter.
+// This sets the header at the bottom of the page
+  $(this).css({
+    bottom:'0'
+  });
+  // This breaks the header by letter and calls the function fadeIn with mouseover
   $('#header03').blast({
-  delimiter:'character'
-}).mouseover(sendToMiddle)
+    delimiter:'character'
+  }).mouseover(sendToMiddle)
 }
-
+// This sends each letter to the centre of the page
 function sendToMiddle(){
   $(this).css({
-    bottom:0
+    top:'50%'
   });
 }
