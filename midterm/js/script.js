6d6ac7954e40000513a1a465d5c15a04a09afcaa
Gavin <gavin.e.park@gmail.com>
2018-02-27 12:04:10 -0500

    Addition of comments, formatting

--- midterm/js/script.js
+++ midterm/js/script.js
@@ -14,7 +14,7 @@ var backgroundFadeTime = 30000;
 // This sets the initial background colour in the array
 var currentBackgroundColor = 0;
 // This is the array of colours that will appear in the background
-var backgroundColors = ['#D29B24','#E9809F','#202344','#0E3D29','#797EB3','#E5B30']
+var backgroundColors = ['#444F3F','#4A4643','#272430','#373740','#282D40','#FA6648']
 
 $(document).ready(function () {
 
@@ -25,76 +25,82 @@ $(document).ready(function () {
   fadeBackground();
   setInterval(fadeBackground,backgroundFadeTime);
 
-// PRIMARY FUNCTIONS //
-
+  // This hides each header until it is called
   $('#header01').hide();
 
-var now = new Date();
-var hour = now.getHours();
-if(hour>=0 && hour<20){
-  dispurse();
-};
+  // This pulls the current date and time
+  var now = new Date();
+  var hour = now.getHours();
+  // This tells each header when it is supposed to appear on the page and calls its function
+  if(hour>=14 && hour<24){
+    dispurse();
+  };
 
-$('#header02').hide();
+  // Same process as above
+  $('#header02').hide();
 
-var now = new Date();
-var hour = now.getHours();
-if(hour>=0 && hour<20){
-disappear();
-};
+  var now = new Date();
+  var hour = now.getHours();
+  if(hour>=8 && hour<13){
+    disappear();
+  };
 
-$('#header03').hide();
+  //same process as above
+  $('#header03').hide();
 
-var now = new Date();
-var hour = now.getHours();
-if(hour>=0 && hour<20){
-rise();
-}
-
-
-
-
-
-
-
-
+  var now = new Date();
+  var hour = now.getHours();
+  if(hour>=0 && hour<7){
+    rise();
+  }
 });
 
+
+// This function calls the background colour to change to the next in the array
 function fadeBackground(){
   currentBackgroundColor=currentBackgroundColor+1;
+  // This tells the function to start back at the first colour when it reaches the end of the array
   if (currentBackgroundColor >= backgroundColors.length){
     currentBackgroundColor = 0;
   }
+  // this uses the variables set at the beginning of the script to animate a fade between background colours
   $('body').animate({
     backgroundColor:backgroundColors[currentBackgroundColor]
   },backgroundFadeTime)
 }
 
+
 // This puts a span around each letter
 function dispurse(){
-$('#header01').show();
-$('#header01').blast({
-  delimiter:'character'
-  //This calls the function animateToRandomLocation when the cursor rolls over each letter
-}).mouseover(animateToRandomLocation);
+  $('#header01').show();
+  $('#header01').blast({
+    delimiter:'character'
+    //This calls the function animateToRandomLocation when the cursor rolls over each letter
+  }).mouseover(animateToRandomLocation);
 };
 
+// This puts a span around each word
 function disappear(){
-$('#header02').show();
-$('#header02').blast({
-  delimiter:'word'
-}).mouseover(wordsFadeOut);
+  $('#header02').show();
+  $('#header02').blast({
+    delimiter:'word'
+    //This calls the function wordsFadeOut when the cursor rolls over each word
+  }).mouseover(wordsFadeOut);
 };
 
-
-  function rise(){
+// This puts a span around each letter
+function rise(){
   $('#header03').show();
   $('#header03').blast({
     delimiter:'character'
+    //This calls the function wordsFloat when the cursor rolls over each letter
   }).mouseover(wordsFloat);
 };
 
+
+// This function sends each letter to a random location on the page
 function animateToRandomLocation () {
+  // This creates a random location on the x axis
   var x = Math.floor(Math.random() * $(window).width());
 
   // Setting the position to be relative keeps the original position of each letter.
@@ -102,11 +108,12 @@ function animateToRandomLocation () {
     position: 'relative'
   });
 
-  // This animation sends each letter to a random location on the bottom of the page
+  // This animation sends each letter to a random location on the x axis and 1600px from the top over 2.5 seconds
   $(this).animate({
     left: x+'px',
     top:'1600px'
   }, 2500, function(){
+    // Once the above function is finished, this animation sends each letter back to its starting location
     $(this).animate({
       top:0,
       left:0,
@@ -114,18 +121,27 @@ function animateToRandomLocation () {
   });
 }
 
+
+// This function makes each word fade in and out on mouseover
 function wordsFadeOut () {
-  $(this).fadeTo(2000,0.04)
-  $(this).fadeTo(1600,1)
+  // This fades to an opactiy of 4% over 2.3 seconds
+  $(this).fadeTo(2300,0.04)
+  // This fades back to an opacty of 100% over 2 seconds
+  $(this).fadeTo(2000,1)
 }
 
+
+// This function makes each letter 'float' on mouseover
 function wordsFloat () {
+  // Setting the position to be relative keeps the original position of each letter
   $(this).css({
     position:'relative'
   });
+  // This animates each letter to 80px above its original position
   $(this).animate({
     top: '-80px'
   });
+  // Once the above animation is finished, this animation sends each letter back to its starting location
   $(this).animate({
     top:'0'
   });
