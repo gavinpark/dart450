f7307cbf6fb3d4b3a27cc59095a1eb3ef14e5fd8
Gavin <gavin.e.park@gmail.com>
2018-02-26 22:11:28 -0500

    addition of .blast() on header01

---
+++ midterm/js/script.js
@@ -0,0 +1,41 @@
+/**********************************************
+
+DART 450, Winter 2018
+Midterm Project
+Gavin Park
+
+Description of what the script does...
+
+**********************************************/
+
+$(document).ready(function () {
+
+  // Insert jQuery code here to run when the page is loaded
+
+  $('#header01').blast({
+    delimiter:'character'
+  }).mouseover(animateToRandomLocation)
+});
+
+
+
+function animateToRandomLocation () {
+  var x = Math.floor(Math.random() * $(window).width());
+
+  // Setting the position to be relative keeps the original position of each letter.
+  $(this).css({
+    position: 'relative'
+  });
+
+  // This animation sends each letter to a random location on the bottom of the page
+  $(this).animate({
+    left: x+'px',
+    top:'2000px'
+  }, 3000, function(){
+    $(this).animate({
+      top:0,
+      left:0,
+    }, 4000);
+  });
+
+}
