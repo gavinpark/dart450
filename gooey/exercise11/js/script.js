71b83b686dd7ae2fb22d965ddd7da4c10fa3f914
Gavin <gavin.e.park@gmail.com>
2018-04-28 23:46:16 -0400

    addition of initial js

--- gooey/exercise11/js/script.js
+++ gooey/exercise11/js/script.js
@@ -8,25 +8,24 @@ Adds a grid of divs to the page
 
 **********************************************/
 
-$(document).ready(function () {
+var slowDiv = document.getElementById("appearDiv");
+//each div or other html element must be "gotten" using get element by ID
+var innerDiv = document.getElementById("innerDiv");
+var movX = 0;
+var movY = 0;
+let opacity = 0;
+//global state
 
-  // Set how many divs we want to add
-  var totalDivsToAdd = 10; 
-  // Keep track of how many we've added in the loop
-  var divsAdded = 0; 
+//mouse move allows us to track when the mouse is moving, because it is classified as a mouse event we need to make a function so that event listener can recognize it.
+function mouseMove() {
+  //this increases the opacity every time the function is called, dividing by 100 makes it "readable" as an opacity value.
+  opacity = opacity + 1 / 500;
 
-  // Keep running the while loop until we've added enough divs
-  while (divsAdded < totalDivsToAdd) { 
-    // Create the div
-    var $div = $('<div></div>');
-    // Add it to the page
-    $('body').append($div);
-    // Make a note that we've added one (so the loop will end at the right time)
-    divsAdded = divsAdded + 1; 
+  //to change css in javascript we can use dot notation to access and manipulate the properties of it's id name which we have already "gotten" using get elementById
+  slowDiv.style.opacity = opacity + "";
+  if (opacity > 1) {
+    innerDiv.style.display = "block";
   }
+}
 
-
-
-
-
-});
+document.getElementById("appearDiv").addEventListener("mousemove", mouseMove);
