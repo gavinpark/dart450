0d59997aa17cc3063c54bf369fb737d83a34554f
Gavin <gavin.e.park@gmail.com>
2018-04-29 20:53:08 -0400

    tried to get second function working without success

--- gooey/exercise11/js/script.js
+++ gooey/exercise11/js/script.js
@@ -8,9 +8,9 @@ Adds a grid of divs to the page
 
 **********************************************/
 
-var slowDiv = document.getElementById("appearDiv01");
+var appearDiv01 = document.getElementById("appearDiv01");
 //This "gets" the appearDiv02 element using get element by ID
-var innerDiv = document.getElementById("appearDiv02");
+var appearDiv02 = document.getElementById("appearDiv02");
 var movX = 0;
 var movY = 0;
 let opacity = 0;
@@ -22,23 +22,13 @@ function mouseMove() {
   opacity = opacity + 1 / 100;
 
   //to change css in javascript we can use dot notation to access and manipulate the properties of it's id name which we have already "gotten" using get elementById
-  slowDiv.style.opacity = opacity + "";
+  appearDiv01.style.opacity = opacity + "";
   if (opacity > 1) {
-  innerDiv.style.opacity = 0.05;
-}
-}
-
-function mouseMove02() {
-  //this increases the opacity every time the function is called, dividing by 100 makes it "readable" as an opacity value.
-  opacity = opacity + 1 / 200;
-
-  //to change css in javascript we can use dot notation to access and manipulate the properties of it's id name which we have already "gotten" using get elementById
-  innerDiv.style.opacity = opacity + "";
-
+      appearDiv02.style.display = "block";
+    }
 }
 
 
 
 
 document.getElementById("appearDiv01").addEventListener("mousemove", mouseMove);
-document.getElementById("appearDiv02").addEventListener("mousemove", mouseMove);
