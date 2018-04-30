1b604214b1c9163302755fbe961814b9babdfc2f
Gavin <gavin.e.park@gmail.com>
2018-04-29 23:06:42 -0400

    added comments

--- gooey/exercise08/js/script.js
+++ gooey/exercise08/js/script.js
@@ -1,11 +1,10 @@
-/**********************************************
+/*
 
 DART 450, Winter 2018
-Exercise 08
+Final Project - gooey
 Gavin Park
 
-
-**********************************************/
+*/
 
 var lastScrollY = 0;
 var lastScrollAmount = 0;
@@ -13,37 +12,45 @@ var lastScrollAmount = 0;
 $(document).ready(function () {
 
   setInterval(function () {
+    // This takes the current location, subtracts the previous location and provides a value
     lastScrollAmount = Math.abs(window.scrollY - lastScrollY);
-    lastScrollY = window.scrollY;
-    disappear();
 
+    // This gets the previous location of the scroll on the y axis
+    lastScrollY = window.scrollY;
+    //this calls the disappear function and animates it over .5 seconds
+    disappear();
   },50);
 
   window.onscroll = function(){
   };
 
-
   function disappear() {
+
+    // This pulls the element's current opacity
     var opacity = $('#paragraph').css('opacity');
     // console.log(opacity);
 
-      if (lastScrollAmount > 25 && opacity == 1){
-        console.log("Animating to 0");
-        $('#paragraph').animate({
-          "opacity": 0,
-        }, 100);
-      } else if (lastScrollAmount <= 25 && opacity == 0) {
-        console.log("Animating to 1");
-        $('#paragraph').css({
-          "opacity": 0.01,
-        })
-        setTimeout (function(){
-          $('#paragraph').animate({
-            "opacity": 1,
-          }, 1500);
-        },1000)
+    // This statement checks if the scroll speed is greater than 25 and if the current opacity is 1
+    if (lastScrollAmount > 25 && opacity == 1){
+      console.log("Animating to 0");
+      // If the above is true, the paragraph will fade to opacity 0 over 1 second
+      $('#paragraph').animate({
+        "opacity": 0,
+      }, 100);
 
-      }
-  // }
+      // This statement checks if the scroll speed is less than 25 but the opacity is 0
+    } else if (lastScrollAmount <= 25 && opacity == 0) {
+      console.log("Animating to 1");
+      // If the above is true, the paragraph will fade to 0.01. this is so that the content never actually disappears
+      $('#paragraph').css({
+        "opacity": 0.01,
+      })
+      // This function animates the paragraph back to 1 over 3 seconds
+      setTimeout (function(){
+        $('#paragraph').animate({
+          "opacity": 1,
+        }, 1500);
+      },3000)
+    }
   }
 });
--- gooey/exercise09/js/script.js
+++ gooey/exercise09/js/script.js
@@ -1,94 +1,90 @@
-/**********************************************
+/*
 
 DART 450, Winter 2018
-Exercise 08
+Final Project - gooey
 Gavin Park
 
-**********************************************/
+*/
 
 // dots is an array of Dot objects,
 // mouse is an object used to track the X and Y position
 
 $(document).ready(function () {
-// This variable represents the dots andn the mouse location
-var dots = [],
-    mouse = {
-      x: 0,
-      y: 0
-    };
 
-// This variable checks the location of the last dot in the array.
-// It makes the last dot in the array trail the position of the cursor
-var Dot = function() {
-  if (dots.length == 0) {
-    this.x = mouse.x;
-    this.y = mouse.y;
-  }
-  else {
-    this.x = dots[dots.length-1].x;
-    this.y = dots[dots.length-1].y;
-  }
-  this.node = (function(){
-    var n = document.createElement("div");
-    n.className = "trail";
-    document.body.appendChild(n);
-    return n;
-  }());
-};
+  // This variable represents the dots and the mouse location
+  var dots = [],
+  mouse = {
+    x: 0,
+    y: 0
+  };
 
-// The Dot.prototype.draw() method sets the position of
-  // the object's <div> node
-Dot.prototype.draw = function() {
-  this.node.style.left = this.x + "px";
-  this.node.style.top = this.y + "px";
-};
+  // This function checks the location of the last dot in the array.
+  // It makes the last dot in the array trail the position of the cursor
+  var Dot = function() {
+    if (dots.length == 0) {
+      this.x = mouse.x;
+      this.y = mouse.y;
+    }
+    else {
+      this.x = dots[dots.length-1].x;
+      this.y = dots[dots.length-1].y;
+    }
+    this.node = (function(){
+      var n = document.createElement("div");
+      n.className = "trail";
+      document.body.appendChild(n);
+      return n;
+    }());
+  };
 
-// This variable creates the dot objects and adds to the dots array
+  // The Dot.prototype.draw() method sets the position of the object's <div> node
+  Dot.prototype.draw = function() {
+    this.node.style.left = this.x + "px";
+    this.node.style.top = this.y + "px";
+  };
 
-var d = new Dot();
-dots.push(d);
+  // This variable is for each new dot
+  var d = new Dot();
+  dots.push(d);
 
-setInterval(function() {
-  // for (var i = 0; i < 1; i++){
+  // This function creates the new dot object every 5 seconds and adds to the dots array
+  setInterval(function() {
     var d = new Dot();
     dots.push(d);
-  // }
-}, 5000);
+  }, 5000);
 
+  // This is the screen redraw function which gets the cursor's location
+  function draw() {
 
-// This is the screen redraw function
-function draw() {
-  // Make sure the mouse position is set everytime
-    // draw() is called.
-  var x = mouse.x,
-      y = mouse.y;
+    var x = mouse.x,
+    y = mouse.y;
 
-  // This loop is where all the 90s magic happens
-  dots.forEach(function(dot, index, dots) {
-    var nextDot = dots[index + 1] || dots[0];
+    // This loop is where all each dot trails the one behind it
+    dots.forEach(function(dot, index, dots) {
+      var nextDot = dots[index + 1] || dots[0];
 
-    dot.x = x;
-    dot.y = y;
-    dot.draw();
-    x += (nextDot.x - dot.x) * .97;
-    y += (nextDot.y - dot.y) * .97;
+      dot.x = x;
+      dot.y = y;
+      dot.draw();
+      x += (nextDot.x - dot.x) * .97;
+      y += (nextDot.y - dot.y) * .97;
 
+    });
+  }
+
+  // This event listener checks for the cursor's location on the page continuously
+  addEventListener("mousemove", function(event) {
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
   });
-}
 
-addEventListener("mousemove", function(event) {
-  //event.preventDefault();
-  mouse.x = event.pageX;
-  mouse.y = event.pageY;
-});
-
-// animate() calls draw() then recursively calls itself
+  // animate() calls draw() then recursively calls itself
   // everytime the screen repaints via requestAnimationFrame().
-function animate() {
-  draw();
-  requestAnimationFrame(animate);
-}
+  function animate() {
+    draw();
+    requestAnimationFrame(animate);
+  }
 
-// And get it started by calling animate().
-animate();
+  // This gets it started by calling animate().
+  animate();
 },3000);
